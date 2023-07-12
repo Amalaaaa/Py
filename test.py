949a6f2602ad3aa3093d8dc91b654005486a818f
Amala <bondadaamala@gmail.com>
2023-07-11 21:37:12 -0400

    oddeven posneg

---
+++ test.py
@@ -0,0 +1,8 @@
+def evenodd():
+    x = range(100)
+    for i in x:
+        if i%2==0:
+            print(i,"is even")
+        else:
+            print(i,"is odd")
+evenodd()           
---
+++ test2.py
@@ -0,0 +1,13 @@
+class posneg:
+    i = float(input("enter a number:"))
+    def positive():
+        if i>0:
+            print(i,"is positive")
+    def negative():
+        if i<0:
+            print(i,"is negative")
+print(posneg.positive)     
+print(posneg.negative)      
+ #numb = posneg()
+#print(posneg.pos())
+#print(posneg.neg())
