ba24e47b103fd26fcc50a1480201a96ce9666906
Amala <bondadaamala@gmail.com>
2023-07-11 21:56:59 -0400

    updated

--- test2.py
+++ test2.py
@@ -1,11 +1,13 @@
 class posneg:
-    i = float(input("enter a number:"))
+    
     def positive():
+
         if i>0:
             print(i,"is positive")
     def negative():
         if i<0:
             print(i,"is negative")
+i = float(input("enter a number:"))         
 print(posneg.positive)     
 print(posneg.negative)      
  #numb = posneg()
